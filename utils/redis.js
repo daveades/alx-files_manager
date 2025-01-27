65b9a1241df5037c4640303ef913bd0e05aed62c
David Adesanya <82074718+daveades@users.noreply.github.com>
2025-01-27 18:46:45 +0000

    Add Redis client utility with basic CRUD operations

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    const value = await asyncGet(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    const asyncSet = promisify(this.client.setex).bind(this.client);
+    await asyncSet(key, duration, value);
+  }
+
+  async del(key) {
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    await asyncDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
